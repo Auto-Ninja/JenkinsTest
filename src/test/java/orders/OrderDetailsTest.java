0834be591fe37daf55e659f677ddc4e28aa4e3b2
SMT <smithajojo@live.com>
2024-10-30 11:23:17 +0000

    Maven test command line added

---
+++ src/test/java/orders/OrderDetailsTest.java
@@ -0,0 +1,15 @@
+package orders;
+
+import org.junit.Test;
+
+public class OrderDetailsTest {
+    @Test
+    public void FirstTest() {
+        System.out.println("First test success....");
+    }
+
+    @Test
+    public void SecondTest() {
+        System.out.println("Second test success....");
+    }
+}
---
+++ src/test/java/orders/OrderTest.java
@@ -0,0 +1,12 @@
+package orders;
+
+import org.junit.Test;
+
+
+public class OrderTest {
+    @Test
+    public void FirstTest(){
+        System.out.println("First test success....");
+    }
+}
+
